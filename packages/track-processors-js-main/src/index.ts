29c649607f77ac2ef50e8f695e800c181b235e96
Slavcho Cvetanovski <cvetanovski.slavco@gmail.com>
2024-11-06 10:07:43 +0100

    chore: added track-processor package in the app
    This MR adds track-processor package locally for modification

---
+++ packages/track-processors-js-main/src/index.ts
@@ -0,0 +1,26 @@
+import ProcessorWrapper from './ProcessorWrapper';
+import BackgroundTransformer, {
+  BackgroundOptions,
+  SegmenterOptions,
+} from './transformers/BackgroundTransformer';
+
+export * from './transformers/types';
+export { default as VideoTransformer } from './transformers/VideoTransformer';
+export { ProcessorWrapper, type BackgroundOptions, type SegmenterOptions, BackgroundTransformer };
+
+export const BackgroundBlur = (blurRadius: number = 10, segmenterOptions?: SegmenterOptions) => {
+  return BackgroundProcessor({ blurRadius, segmenterOptions }, 'background-blur');
+};
+
+export const VirtualBackground = (imagePath: string, segmenterOptions?: SegmenterOptions) => {
+  return BackgroundProcessor({ imagePath, segmenterOptions }, 'virtual-background');
+};
+
+export const BackgroundProcessor = (options: BackgroundOptions, name = 'background-processor') => {
+  const isProcessorSupported = ProcessorWrapper.isSupported && BackgroundTransformer.isSupported;
+  if (!isProcessorSupported) {
+    throw new Error('processor is not supported in this browser');
+  }
+  const processor = new ProcessorWrapper(new BackgroundTransformer(options), name);
+  return processor;
+};
