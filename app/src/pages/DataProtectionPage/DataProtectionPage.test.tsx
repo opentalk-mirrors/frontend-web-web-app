8aa7677e316f2027164c5f8398db81b12439c756
Michael Holst <m.holst@opentalk.eu>
2025-03-19 16:15:32 +0100

    test(unit): add test for DataProtectionPage component

---
+++ app/src/pages/DataProtectionPage/DataProtectionPage.test.tsx
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+import { render, screen } from '@testing-library/react';
+
+import { useUpdateDocumentTitle } from '../../hooks/useUpdateDocumentTitle';
+import { DataProtectionPage } from './DataProtectionPage';
+
+jest.mock('../../hooks/useUpdateDocumentTitle', () => ({
+  useUpdateDocumentTitle: jest.fn(),
+}));
+
+describe('DataProtectionPage', () => {
+  test('renders without crash', () => {
+    render(<DataProtectionPage />);
+
+    expect(screen.getByRole('heading', { name: 'dashboard-legal-data-protection' })).toBeInTheDocument();
+    expect(useUpdateDocumentTitle).toHaveBeenCalledTimes(1);
+    expect(useUpdateDocumentTitle).toHaveBeenCalledWith('dashboard-legal-data-protection');
+  });
+});
