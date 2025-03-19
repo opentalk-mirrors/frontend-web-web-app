51c8446808eb4d010ca5f347fdb5cfa031ccaafa
Michael Holst <m.holst@opentalk.eu>
2025-03-19 15:59:42 +0100

    test(unit): add test for ImprintPage component

---
+++ app/src/pages/ImprintPage/ImprintPage.test.tsx
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+import { render, screen } from '@testing-library/react';
+
+import { useUpdateDocumentTitle } from '../../hooks/useUpdateDocumentTitle';
+import { ImprintPage } from './ImprintPage';
+
+jest.mock('../../hooks/useUpdateDocumentTitle', () => ({
+  useUpdateDocumentTitle: jest.fn(),
+}));
+
+describe('ImprintPage', () => {
+  test('renders without crash', () => {
+    render(<ImprintPage />);
+
+    expect(screen.getByRole('heading', { name: 'dashboard-legal-imprint' })).toBeInTheDocument();
+    expect(useUpdateDocumentTitle).toHaveBeenCalledTimes(1);
+    expect(useUpdateDocumentTitle).toHaveBeenCalledWith('dashboard-legal-imprint');
+  });
+});
