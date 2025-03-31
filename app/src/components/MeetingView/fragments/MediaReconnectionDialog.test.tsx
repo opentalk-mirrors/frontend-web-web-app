954ff517dbc77cfbb2293b2832d58eefd5fadee0
Maximilian Fuß <m.fuss@opentalk.eu>
2025-03-31 16:33:10 +0200

    test: extend MediaReconnectionDialog test

--- app/src/components/MeetingView/fragments/MediaReconnectionDialog.test.tsx
+++ app/src/components/MeetingView/fragments/MediaReconnectionDialog.test.tsx
@@ -1,18 +1,31 @@
 // SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
 //
 // SPDX-License-Identifier: EUPL-1.2
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 import { configureStore, renderWithProviders } from '../../../utils/testUtils';
 import MediaReconnectionDialog from './MediaReconnectionDialog';
 
 describe('MediaReconnectionDialog', () => {
-  test('will render without errors', async () => {
-    const { store } = configureStore();
+  const { store, dispatchSpy } = configureStore();
 
+  test('will render without errors', async () => {
     renderWithProviders(<MediaReconnectionDialog />, { store, provider: { mui: true } });
 
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'breakout-room-notification-button-leave' })).toBeInTheDocument();
+  });
+
+  test('will call hangup', async () => {
+    renderWithProviders(<MediaReconnectionDialog />, { store, provider: { mui: true } });
+
+    fireEvent.click(screen.getByRole('button', { name: 'breakout-room-notification-button-leave' }));
+
+    const dispatchedThunk = dispatchSpy.mock.calls[0][0];
+    const dispatch = jest.fn();
+    await dispatchedThunk(dispatch, store.getState, undefined);
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({ type: 'room/hangup/pending' }));
   });
 });
