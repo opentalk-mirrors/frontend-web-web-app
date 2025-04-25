f7b520a0f4bb17ee722d2252739f47beb14c0deb
k.koebel <325-k.koebel@users.noreply.gitlab.heinlein-video.de>
2025-04-25 12:13:50 +0545

    test(e2e): adding e2e tests for TC_005_VideoRoom_ParticipantViewSettings_List_Sorting

--- e2e/tests/pages/MeetingRoomPage.ts
+++ e2e/tests/pages/MeetingRoomPage.ts
@@ -8,17 +8,6 @@ export class MeetingRoomPage {
 
   meetingRoomName: Locator;
 
-  toolBar: {
-    handRaiseButton: Locator;
-    turnOnScreenShareButton: Locator;
-    microphoneButton: Locator;
-    microphoneMoreOptionsMenuButton: Locator;
-    videoButton: Locator;
-    cameraMoreOptionButton: Locator;
-    moreOptionButton: Locator;
-    endMeetingButton: Locator;
-  };
-
   viewOptions: {
     viewOptionsButton: Locator;
     viewAndSortingPopupMenu: Locator;
@@ -53,6 +42,22 @@ export class MeetingRoomPage {
     debriefingButton: Locator;
   };
 
+  videoPreview: Locator;
+  videoPreviewName: Locator;
+
+  toolBar: {
+    handRaiseButton: Locator;
+    turnOnScreenShareButton: Locator;
+    microphoneButton: Locator;
+    microphoneButtonOff: Locator;
+    microphoneMoreOptionsMenuButton: Locator;
+    videoButton: Locator;
+    videoButtonOff: Locator;
+    cameraMoreOptionButton: Locator;
+    moreOptionButton: Locator;
+    endMeetingButton: Locator;
+  };
+
   chatButton: Locator;
   peopleButton: Locator;
   messageButton: Locator;
@@ -78,17 +83,6 @@ export class MeetingRoomPage {
   constructor({ page }: { page: Page }) {
     this.page = page;
     this.meetingRoomName = this.page.locator('h1').first();
-
-    this.toolBar = {
-      handRaiseButton: this.page.getByRole('button', { name: 'Raise Your Hand' }),
-      turnOnScreenShareButton: this.page.getByRole('button', { name: 'Turn On Screen Share' }),
-      microphoneButton: this.page.getByRole('button', { name: 'Turn On Audio' }),
-      microphoneMoreOptionsMenuButton: this.page.getByRole('button', { name: 'additional options microphone' }),
-      videoButton: this.page.getByRole('button', { name: 'Turn On Video' }),
-      cameraMoreOptionButton: this.page.getByRole('button', { name: 'additional options camera' }),
-      moreOptionButton: this.page.getByRole('button', { name: 'More Options' }),
-      endMeetingButton: this.page.getByRole('button', { name: 'Leave Call' }),
-    };
 
     this.viewOptions = {
       viewOptionsButton: this.page.getByRole('button', { name: 'Select view' }),
@@ -125,6 +119,27 @@ export class MeetingRoomPage {
       debriefingButton: this.page.getByRole('tab', { name: 'Debriefing' }),
     };
 
+    this.videoPreview = this.page.getByRole('complementary', { name: 'Tools' }).locator('video');
+    // video container that is nested inside 'aside' tag --> complementary is the role of the aside, see https://www.w3.org/TR/html-aria/#docconformance
+
+    this.videoPreviewName = this.page
+      .getByRole('complementary', { name: 'Tools' }) // element doesn't seem to be nested in the aside on CI
+      .getByTestId(this.selectors.participantName);
+    //.first();
+
+    this.toolBar = {
+      handRaiseButton: this.page.getByRole('button', { name: 'Raise Your Hand' }),
+      turnOnScreenShareButton: this.page.getByRole('button', { name: 'Turn On Screen Share' }),
+      microphoneButton: this.page.getByRole('button', { name: 'Turn On Audio' }),
+      microphoneButtonOff: this.page.getByRole('button', { name: 'Turn Off Audio' }),
+      microphoneMoreOptionsMenuButton: this.page.getByRole('button', { name: 'additional options microphone' }),
+      videoButton: this.page.getByRole('button', { name: 'Turn On Video' }),
+      videoButtonOff: this.page.getByRole('button', { name: 'Turn Off Video' }),
+      cameraMoreOptionButton: this.page.getByRole('button', { name: 'additional options camera' }),
+      moreOptionButton: this.page.getByRole('button', { name: 'More Options' }),
+      endMeetingButton: this.page.getByRole('button', { name: 'Leave Call' }),
+    };
+
     this.chatButton = this.page.getByRole('tab', { name: 'Chat' });
     this.peopleButton = this.page.getByRole('tab', { name: 'People' });
     this.messageButton = this.page.getByRole('tab', { name: 'Messages' });
@@ -133,16 +148,6 @@ export class MeetingRoomPage {
     this.chatTextField = this.page.getByPlaceholder('Type a message');
     this.chatSubmitButton = this.page.getByRole('button', { name: 'submit chat message' });
 
-    this.toolBar = {
-      handRaiseButton: this.page.getByRole('button', { name: 'Raise Your Hand' }),
-      turnOnScreenShareButton: this.page.getByRole('button', { name: 'Turn On Screen Share' }),
-      microphoneButton: this.page.getByRole('button', { name: 'Turn On Audio' }),
-      microphoneMoreOptionsMenuButton: this.page.getByRole('button', { name: 'additional options microphone' }),
-      videoButton: this.page.getByRole('button', { name: 'Turn On Video' }),
-      cameraMoreOptionButton: this.page.getByRole('button', { name: 'additional options camera' }),
-      moreOptionButton: this.page.getByRole('button', { name: 'More Options' }),
-      endMeetingButton: this.page.getByRole('button', { name: 'Leave Call' }),
-    };
     this.burgerMenuButton = this.page.getByRole('button', { name: 'My meeting', exact: true });
     this.securityMonitorButton = this.page.getByRole('button', { name: 'Show security monitor' });
   }
@@ -158,10 +163,34 @@ export class MeetingRoomPage {
     }
   }
 
+  async renderMeetingRoom(): Promise<void> {
+    await this.page.waitForLoadState();
+    await this.meetingRoomName.waitFor({ state: 'visible' });
+  }
+
   async getMeetingRoomName(): Promise<string> {
+    await this.meetingRoomName.waitFor();
     return await this.meetingRoomName.textContent();
   }
 
+  async getUserName(): Promise<string> {
+    let userName = '';
+    // user name is only visible if camera is turned on
+    const initialCameraStatus = await this.isCameraOn();
+    if (!initialCameraStatus) {
+      await this.turnCameraOn();
+    }
+    if (await this.videoPreviewName.isVisible()) {
+      userName = await this.videoPreviewName.innerText();
+    }
+    if (!initialCameraStatus) {
+      // reset to initial camera status
+      await this.turnCameraOff();
+    }
+    return userName;
+  }
+
+  // functions related to view options menu
   async displayViewOptionsMenu(): Promise<void> {
     await this.viewOptions.viewOptionsButton.waitFor();
     await this.viewOptions.viewOptionsButton.click();
@@ -170,20 +199,71 @@ export class MeetingRoomPage {
   }
 
   async selectGridViewOption(): Promise<void> {
+    await this.viewOptions.gridViewOption.waitFor();
     await this.viewOptions.gridViewOption.click();
   }
 
   async selectSpeakerViewOption(): Promise<void> {
+    await this.viewOptions.speakerViewOption.waitFor();
     await this.viewOptions.speakerViewOption.click();
   }
 
-  async selectPeopleTab(): Promise<void> {
-    await this.peopleButton.click();
+  async selectActivatedCameraFirstSortingOption(): Promise<void> {
+    await this.viewOptions.activatedCameraFirstSortingOption.waitFor();
+    await this.viewOptions.activatedCameraFirstSortingOption.click();
+  }
+
+  async selectModertorsFirstSortingOption(): Promise<void> {
+    await this.viewOptions.moderatorsFirstSortingOption.waitFor();
+    await this.viewOptions.moderatorsFirstSortingOption.click();
   }
 
   async hasTickIcon(element: Locator): Promise<boolean> {
     // if menu item has a tick, count should be 1, else 0
     return (await element.locator('div').first().locator('svg').count()) === 1;
+  }
+
+  // toolbar functions
+  async isAudioOn(): Promise<boolean> {
+    return await this.toolBar.microphoneButtonOff.isVisible();
+  }
+
+  async isCameraOn(): Promise<boolean> {
+    return await this.videoPreview.isVisible();
+  }
+
+  async turnAudioOn(): Promise<boolean> {
+    await this.toolBar.microphoneButton.isVisible();
+    await this.toolBar.microphoneButton.click();
+    await this.page.waitForTimeout(1000); // to make sure microphone is really activated
+    return await this.toolBar.videoButtonOff.isVisible();
+  }
+
+  async turnAudioOff(): Promise<boolean> {
+    await this.toolBar.microphoneButtonOff.isVisible();
+    await this.toolBar.microphoneButtonOff.click();
+    await this.page.waitForTimeout(1000); // to make sure microphone is really deactivated
+    return await this.toolBar.microphoneButton.isVisible();
+  }
+
+  async turnCameraOn(): Promise<boolean> {
+    await this.toolBar.videoButton.isVisible();
+    await this.toolBar.videoButton.click();
+    await this.page.waitForTimeout(1000); // to make sure camera is really activated
+    // it seems that in firefox, one has to give approval for use of camera (click 'Allow' in corresponding popup)
+    return await this.toolBar.videoButtonOff.isVisible();
+  }
+
+  async turnCameraOff(): Promise<boolean> {
+    await this.toolBar.videoButtonOff.isVisible();
+    await this.toolBar.videoButtonOff.click();
+    await this.page.waitForTimeout(1000); // to make sure camera is really deactivated
+    return await this.toolBar.videoButton.isVisible();
+  }
+
+  async selectPeopleTab(): Promise<void> {
+    await this.peopleButton.waitFor();
+    await this.peopleButton.click();
   }
 
   async getNumberOfParticipantsInMeeting(): Promise<number> {
@@ -192,12 +272,13 @@ export class MeetingRoomPage {
     return +numberOfParticipants.slice(1, numberOfParticipants.length - 1);
   }
 
+  // meeting room functions (related to how particpants are displayed)
   async pinNthParticipantInSpeakerView(nth: number): Promise<string> {
     const participantsThumbs = await this.page.getByTestId(this.selectors.speakerViewParticipantsThumbsHolder);
     const nthParticipantWindow = await participantsThumbs.getByTestId(this.selectors.participantWindow).nth(nth - 1); // minus 1 because nth(0) is the first element
-    const nthParticipantName = await nthParticipantWindow.getByTestId(this.selectors.participantName).innerText();
+    const nthParticipantNameTile = await nthParticipantWindow.getByTestId(this.selectors.participantName);
     await nthParticipantWindow.click();
-    return nthParticipantName;
+    return await this.getNameTileText(nthParticipantNameTile);
   }
 
   async getPinnedParticipantNameInSpeakerView(): Promise<string> {
@@ -205,31 +286,49 @@ export class MeetingRoomPage {
       .getByTestId(this.selectors.speakerWindow)
       .getByTestId(this.selectors.participantWindow);
     const speakerNameTile = await speakerWindow.getByTestId(this.selectors.participantName);
-    if (await speakerNameTile.isVisible()) {
-      return await speakerNameTile.innerText();
-    } else {
-      return '';
-    }
+    return await this.getNameTileText(speakerNameTile);
   }
 
   async getFirstParticipantNameInSpeakerView(): Promise<string> {
-    const participantName = await this.page
+    const participantNameTile = await this.page
       .getByTestId(this.selectors.speakerViewContainer)
       .getByTestId(this.selectors.participantWindow)
       .first()
-      .getByTestId(this.selectors.participantName)
-      .innerText();
-    return participantName;
+      .getByTestId(this.selectors.participantName);
+    return await this.getNameTileText(participantNameTile);
   }
 
   async getThumbsNthParticipantNameInSpeakerView(nth: number): Promise<string> {
-    const participantName = await this.page
+    const participantWindow = await this.page
       .getByTestId(this.selectors.speakerViewParticipantsThumbsHolder)
       .getByTestId(this.selectors.participantWindow)
-      .nth(nth - 1) // minus 1 because nth(0) is the first element
-      .getByTestId(this.selectors.participantName)
-      .innerText();
-    return participantName;
+      .nth(nth - 1); // minus 1 because nth(0) is the first element
+    const participantNameTile = await participantWindow.getByTestId(this.selectors.participantName);
+    return await this.getNameTileText(participantNameTile);
+  }
+
+  async getNthParticipantNameInGridView(nth: number): Promise<string> {
+    const participantWindow = await this.page
+      //.getByTestId(this.selectors.gridViewContainer) // current version on CI doesn't have 'grid-container' test ID
+      .getByTestId(this.selectors.participantWindow)
+      .nth(nth - 1); // minus 1 because nth(0) is the first element
+    const participantNameTile = await participantWindow.getByTestId(this.selectors.participantName);
+    return await this.getNameTileText(participantNameTile);
+  }
+
+  async getNameTileText(nameTile: Locator): Promise<string> {
+    let nameTileText = '';
+    if (await nameTile.isVisible()) {
+      nameTileText = await nameTile.innerText();
+    }
+    return nameTileText;
+  }
+
+  async isGridViewNthParticipantCameraOn(nth: number): Promise<boolean> {
+    const isGridViewParticipantCameraOn = await this.viewOptions.gridViewParticipantWindow
+      .nth(nth - 1) // minus 1 because nth(0) is the first
+      .locator('video');
+    return await isGridViewParticipantCameraOn.isVisible();
   }
 
   async getGridViewNthParticipantWindowAlignment(nth: number): Promise<string> {
@@ -248,10 +347,5 @@ export class MeetingRoomPage {
         return window.getComputedStyle(el).getPropertyValue('width'); // only evaluating width, same could be done with height
       });
     return gridViewParticipantWindowSize;
-  }
-
-  async renderMeetingRoom(): Promise<void> {
-    await this.page.waitForLoadState();
-    await this.meetingRoomName.waitFor({ state: 'visible' });
   }
 }
