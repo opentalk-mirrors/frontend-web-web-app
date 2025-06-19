b6756b470433f3e1dc635321f8b663be9d6a01ee
Ashim Shrestha <ashim@jankaritech.com>
2025-06-19 17:36:24 +0545

    test(e2e): tc_004_meeting room_as moderator_timer_create timer_without title
    Signed-off-by: Ashim Shrestha <ashim@jankaritech.com>

--- e2e/tests/e2e/meetingRoom/meetingRoomModeratorSidebar/meetingRoomTimer.spec.ts
+++ e2e/tests/e2e/meetingRoom/meetingRoomModeratorSidebar/meetingRoomTimer.spec.ts
@@ -3,25 +3,29 @@
 // SPDX-License-Identifier: EUPL-1.2
 import { test, expect } from '@playwright/test';
 
-import {
-  joinMeetingRoomAsGuest,
-  joinMeetingRoomWithNGuests,
-  startAdhocMeetingAsModerator,
-} from '../../../helper/meetingHelpers';
+import { joinMeetingRoomAsGuest, startAdhocMeetingAsModerator } from '../../../helper/meetingHelpers';
 import { MeetingRoomPage } from '../../../pages/MeetingRoom/MeetingRoomPage';
 
 const timerTitle = 'MyTimer';
 const NUMBER_OF_GUESTS = 1;
 
 test.describe('Meeting Room_Timer', () => {
-  test('TC_001_Meeting Room_As Moderator_Timer', async ({ page, context, browserName }) => {
-    test.skip(browserName === 'webkit');
+  let meetingRoomPage: MeetingRoomPage,
+    guestLink: string,
+    guestMeetingRoomPage: MeetingRoomPage,
+    meetingParticipantPages: MeetingRoomPage[];
 
-    const { meetingRoomPage, guestLink } = await startAdhocMeetingAsModerator(page);
-    await joinMeetingRoomWithNGuests(page, context, guestLink, 'guest', 2);
-
+  test.beforeEach(async ({ page, context }) => {
+    ({ meetingRoomPage, guestLink } = await startAdhocMeetingAsModerator(page));
+    guestMeetingRoomPage = await joinMeetingRoomAsGuest(context, guestLink, 'guest');
+    meetingParticipantPages = [meetingRoomPage, guestMeetingRoomPage]; // need to add invitedGuest in future
     await meetingRoomPage.page.bringToFront();
     await meetingRoomPage.startTimerModeratorTool();
+  });
+
+  test('TC_001_Meeting Room_As Moderator_Timer', async ({ browserName }) => {
+    test.skip(browserName === 'webkit');
+
     await expect(meetingRoomPage.timer.timerHeading).toHaveText('Timer');
     await expect(meetingRoomPage.timer.duration.durationSelectionButton).toHaveAccessibleName('Duration 1 minute');
     await expect(meetingRoomPage.timer.titleTextbox).toBeVisible();
@@ -29,12 +33,7 @@ test.describe('Meeting Room_Timer', () => {
     await expect(meetingRoomPage.timer.createTimer.createTimerButton).toBeVisible();
   });
 
-  test('TC_002_Meeting Room_As Moderator_Timer_Duration_Session Duration', async ({ page, context }) => {
-    const { meetingRoomPage, guestLink } = await startAdhocMeetingAsModerator(page);
-    await joinMeetingRoomWithNGuests(page, context, guestLink, 'guest', 2);
-    await meetingRoomPage.page.bringToFront();
-    await meetingRoomPage.startTimerModeratorTool();
-
+  test('TC_002_Meeting Room_As Moderator_Timer_Duration_Session Duration', async () => {
     await meetingRoomPage.openDurationSelection();
     await expect(meetingRoomPage.timer.duration.sessionDurationTitle).toHaveText('Session Duration');
     await expect(meetingRoomPage.timer.duration.unlimitedTimeButton).toBeVisible();
@@ -83,19 +82,6 @@ test.describe('Meeting Room_Timer', () => {
   });
 
   test.describe('TC_003_Meeting Room_As Moderator_Timer_Create Timer_with different duration, with Title', () => {
-    let meetingRoomPage: MeetingRoomPage,
-      guestLink: string,
-      guestMeetingRoomPage: MeetingRoomPage,
-      meetingParticipantPages: MeetingRoomPage[];
-
-    test.beforeEach(async ({ page, context }) => {
-      ({ meetingRoomPage, guestLink } = await startAdhocMeetingAsModerator(page));
-      guestMeetingRoomPage = await joinMeetingRoomAsGuest(context, guestLink, 'guest');
-      meetingParticipantPages = [meetingRoomPage, guestMeetingRoomPage]; // need to add invitedGuest in future
-      await meetingRoomPage.page.bringToFront();
-      await meetingRoomPage.startTimerModeratorTool();
-    });
-
     test('Create Timer (With Unlimited Time)', async ({ browserName }) => {
       test.skip(browserName === 'webkit');
 
@@ -249,5 +235,52 @@ test.describe('Meeting Room_Timer', () => {
       await expect(meetingRoomPage.timer.createTimer.createTimerButton).toBeVisible();
       await expect(meetingRoomPage.moderationTools.coffeeBreakButton).toBeEnabled();
     });
+  });
+
+  test('TC_004_Meeting Room_As Moderator_Timer_Create Timer_without Title', async ({ browserName }) => {
+    test.skip(browserName === 'webkit');
+
+    await meetingRoomPage.openDurationSelection();
+    await meetingRoomPage.selectTimerDuration('oneMinute');
+    await meetingRoomPage.saveSessionDuration();
+    await expect(meetingRoomPage.timer.duration.sessionDurationPopup).not.toBeVisible();
+    await expect(meetingRoomPage.timer.duration.durationSelectionButton).toHaveAccessibleName('Duration 1 minute');
+
+    expect(await meetingRoomPage.getTimerTitleInputValue()).toBe('');
+
+    await meetingRoomPage.toggleAskParticipantsIfReady(true);
+    await expect(meetingRoomPage.timer.participantsReadyCheckbox).toBeChecked();
+
+    await meetingRoomPage.createTimer();
+    await expect(meetingRoomPage.getTimerStartedPopup()).toBeVisible();
+    for (const page of meetingParticipantPages) {
+      await page.page.bringToFront();
+      await expect(page.timer.createTimer.timerStartedPopup.timerStartedHeading).toHaveText('A timer was started');
+      await expect(page.timer.createTimer.timerStartedPopup.remainingTimeLabel).toBeVisible();
+      await expect(page.timer.createTimer.timerStartedPopup.time).toBeVisible();
+      expect(await page.isCountingUp(page.timer.createTimer.timerStartedPopup.time)).toBeFalsy();
+      await expect(page.timer.createTimer.timerStartedPopup.markMeAsDoneButton).toBeVisible();
+    }
+
+    await meetingRoomPage.page.bringToFront();
+    await expect(meetingRoomPage.timer.createTimer.tabPanel.heading).toHaveText('Timer');
+    await expect(meetingRoomPage.timer.createTimer.tabPanel.remainingTimeLabel).toBeVisible();
+    await expect(meetingRoomPage.timer.createTimer.tabPanel.time).toBeVisible();
+    expect(await meetingRoomPage.isCountingUp(meetingRoomPage.timer.createTimer.tabPanel.time)).toBeFalsy();
+    await expect(meetingRoomPage.timer.createTimer.tabPanel.participantsHeading).toBeVisible();
+    expect(await meetingRoomPage.getParticipantsNotDoneStatus()).toHaveLength(NUMBER_OF_GUESTS);
+    await expect(meetingRoomPage.timer.createTimer.stopTimerButton).toBeVisible();
+    await expect(meetingRoomPage.moderationTools.coffeeBreakButton).toBeDisabled();
+
+    await meetingRoomPage.waitForRemainingTimerTime();
+    for (const page of meetingParticipantPages) {
+      await page.page.bringToFront();
+      await expect(page.getTimerStartedPopup()).not.toBeVisible();
+      await expect(page.timer.createTimer.timerRanOutAlert).toBeVisible();
+    }
+
+    await meetingRoomPage.page.bringToFront();
+    await expect(meetingRoomPage.timer.createTimer.createTimerButton).toBeVisible();
+    await expect(meetingRoomPage.moderationTools.coffeeBreakButton).toBeEnabled();
   });
 });
