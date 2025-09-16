bc3026bbde10cc427452e943720ebfa08bf17ac6
Stefan Latsarevich <stefan.l@droxic.com>
2025-09-16 10:55:06 +0200

    chore: add missing coffee break curtain on mobile

--- app/src/components/MeetingView/Mobile/MobileInnerLayout.tsx
+++ app/src/components/MeetingView/Mobile/MobileInnerLayout.tsx
@@ -1,15 +1,20 @@
 // SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
 //
 // SPDX-License-Identifier: EUPL-1.2
+import { useAppSelector } from '../../../hooks';
+import { selectShowCoffeeBreakCurtain } from '../../../store/slices/uiSlice';
+import { CoffeeBreakMobileView } from '../../CoffeeBreakView/CoffeeBreakMobileView';
 import MeetingHeader from '../../MeetingHeader';
 import Toolbar from '../../Toolbar';
 import MobileCinemaContainer from './MobileCinemaContainer';
 
 const MobileInnerLayout = () => {
+  const showCoffeeBreakCurtain = useAppSelector(selectShowCoffeeBreakCurtain);
+
   return (
     <>
       <MeetingHeader />
-      <MobileCinemaContainer />
+      {showCoffeeBreakCurtain ? <CoffeeBreakMobileView /> : <MobileCinemaContainer />}
       <Toolbar />
     </>
   );
