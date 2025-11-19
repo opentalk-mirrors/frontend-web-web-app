0cd71ab1e403cd8b4c7448b38a052902524dde63
Michael Holst <m.holst@opentalk.eu>
2025-11-19 12:29:57 +0100

    chore(eslint): fix eslint issues for DesktopHome

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -93,7 +93,6 @@ export default defineConfig([
       'app/src/pages/Dashboard/CreateEventsPage/CreateEventsPage.tsx',
       'app/src/pages/Dashboard/EditEventsPage/EditEventsPage.tsx',
       'app/src/pages/Dashboard/Home/Home.tsx',
-      'app/src/pages/Dashboard/Home/fragments/DesktopHome.tsx',
       'app/src/pages/ExtendedTabPage/fragments/Video.tsx',
       'app/src/pages/ExtendedTabPage/hooks/useBroadcastChannel.ts',
       'app/src/provider/BreakoutRoomProvider/fragments/BreakoutRoomNotification.tsx',
