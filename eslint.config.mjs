2126bca1d708ae56cb95670129139c0cf37fc36d
Michael Holst <m.holst@opentalk.eu>
2025-12-11 11:07:39 +0100

    chore(eslint): fix eslint for CoffeeBreakTimer
    closes #2855

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -38,7 +38,6 @@ export default defineConfig([
       'app/src/components/Chat/Chat.tsx',
       'app/src/components/Chat/fragments/ChatAnnouncement.tsx',
       'app/src/components/Chat/fragments/ChatMessage.tsx',
-      'app/src/components/CoffeeBreakView/fragments/CoffeeBreakTimer.tsx',
       'app/src/components/LegalVoteTab/fragments/CreateLegalVoteForm.tsx',
       'app/src/components/LegalVoteTab/fragments/ParticipantSelector.tsx',
       'app/src/components/MeetingHeader/fragments/SharedFolderPopover.tsx',
