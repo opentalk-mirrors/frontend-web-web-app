ad76128e366a0cc1a55496500d4706aeb72b78fe
Maximilian Fuß <m.fuss@opentalk.eu>
2026-02-20 17:55:04 +0100

    fix: unify loading indicator ux during app initialization
    Closes #3185

--- app/src/pages/SplashScreenPage/SplashScreenPage.tsx
+++ app/src/pages/SplashScreenPage/SplashScreenPage.tsx
@@ -1,8 +1,23 @@
 // SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
 //
 // SPDX-License-Identifier: EUPL-1.2
+import { ThemeProvider } from '@mui/material';
+
+import { createOpenTalkTheme } from '../../assets/themes/opentalk';
+import { defaultDarkModeColors, defaultLightModeColors } from '../../assets/themes/opentalk/palette';
 import SuspenseLoading from '../../commonComponents/SuspenseLoading/SuspenseLoading';
 
-const SplashScreenPage = () => <SuspenseLoading />;
+const bootstrapTheme = createOpenTalkTheme('light', {
+  light: defaultLightModeColors,
+  dark: defaultDarkModeColors,
+});
+
+const SplashScreenPage = () => (
+  <ThemeProvider theme={bootstrapTheme}>
+    <div className="loading-container">
+      <SuspenseLoading size="64px" color="secondary" />
+    </div>
+  </ThemeProvider>
+);
 
 export default SplashScreenPage;
