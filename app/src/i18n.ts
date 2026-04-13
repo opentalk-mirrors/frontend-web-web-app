ff97113cc920baff9ea6790c2a204db44c443811
Maximilian Fuß <m.fuss@opentalk.eu>
2026-04-13 10:20:15 +0200

    chore(deps): pnpm update

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -20,7 +20,6 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    showSupportNotice: false,
     fallbackLng: 'en',
     supportedLngs: ['de', 'en'],
     nonExplicitSupportedLngs: true,
