d402e15fc5c1d8640b16829c8e0c99d73711bff3
Michael Holst <m.holst@opentalk.eu>
2026-06-05 17:32:38 +0200

    fix(storage): add license header

--- app/src/components/StorageTooltip/StorageTooltip.tsx
+++ app/src/components/StorageTooltip/StorageTooltip.tsx
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
 import { Link as MUILink, Tooltip } from '@mui/material';
 import { Trans } from 'react-i18next';
 
